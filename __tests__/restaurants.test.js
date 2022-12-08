fb1cfa7335e944b053a5ad62444ac83d1e530654
Emily Pisaygnane <episaygnane@me.com>
2022-12-08 13:11:50 -0800

    fixed the bugs, delete review passes

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -11,6 +11,18 @@ const mockUser = {
   password: '12345',
 };
 
+const mockAdmin = {
+  email: 'admin',
+  firstName: 'admin',
+  lastName: 'admin',
+  password: 'admin',
+};
+
+const mockReview = {
+  stars: 4,
+  detail: 'ew',
+};
+
 const registerAndLogin = async (userProps = {}) => {
   const password = userProps.password ?? mockUser.password;
   const agent = request.agent(app);
@@ -82,20 +94,23 @@ describe('restaurant routes', () => {
           Object {
             "detail": "Best restaurant ever!",
             "id": "1",
+            "restaurant_id": "1",
             "stars": 5,
-            "user_id": "1",
+            "userId": "1",
           },
           Object {
             "detail": "Terrible service :(",
             "id": "2",
+            "restaurant_id": "1",
             "stars": 1,
-            "user_id": "2",
+            "userId": "2",
           },
           Object {
             "detail": "It was fine.",
             "id": "3",
+            "restaurant_id": "1",
             "stars": 4,
-            "user_id": "3",
+            "userId": "3",
           },
         ],
         "website": "http://www.PipsOriginal.com",
@@ -113,19 +128,25 @@ describe('restaurant routes', () => {
       Object {
         "detail": "This is a new review!!!",
         "id": "4",
+        "restaurant_id": "1",
         "stars": null,
-        "user_id": null,
+        "userId": "4",
       }
     `);
   });
 
-  it('DELETE /api/v1/reviews/:id should delete a review', async () => {
-    const [agent] = await registerAndLogin();
-    const res = await agent.delete('/api/v1/reviews/1');
+  it('DELETE /api/v1/restaurants/:id/reviews should delete a review', async () => {
+    const [agent] = await registerAndLogin(mockAdmin);
+
+    const review = await agent
+      .post('/api/v1/restaurants/3/reviews')
+      .send(mockReview);
+
+    const res = await agent.delete(`/api/v1/reviews/${review.body.id}`);
     expect(res.status).toBe(200);
 
-    const reviewRes = await agent.get('/api/v1/reviews/1');
-    expect(reviewRes.status).toBe(404);
+    const getRes = await request(app).get(`/api/v1/reviews/${review.body.id}`);
+    expect(getRes.status).toBe(404);
   });
 
   afterAll(async () => {
